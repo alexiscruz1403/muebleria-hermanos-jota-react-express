a7c01b57a3536f889dacec6a01287d2915d4664e
Emir1481 <emirnahuel@gmail.com>
2025-10-04 07:18:44 -0300

    middlewar listo

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import { logger } from './middlewares/logger.js';
 
 import productsRouter from './routes/products.routes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// Middlewares --> no se si estan bien puestos aca
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.originalUrl}`);
-  next();
-});
+app.use(logger);
 
 // Rutas
 app.use('/api/productos', productsRouter);
